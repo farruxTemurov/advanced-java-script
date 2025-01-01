63d80f9f554f3ef899e5c9ff4553e247ae68aba7
Farrux Temurov <tfarrux70@gmail.com>
2025-01-01 20:31:07 +0500

    new changes to food-lover-site with additon of contact-us modal and its functionalities

--- the-food-lover-site/js/script.js
+++ the-food-lover-site/js/script.js
@@ -108,4 +108,44 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline)
+
+    // Modals
+    const modalOpenBtns = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector("[data-modal-close]"),
+        modalContent = document.querySelector(".modal__content")
+
+    function openModal() {
+        modalContent.classList.add("modal_fade")
+        modal.classList.add("show")
+        modal.classList.remove("hide")
+        document.body.style.overflow = "hidden"
+        clearInterval(modalTimerId)
+    }
+
+    function closeModal() {
+        modal.classList.add("hide")
+        modal.classList.remove("show")
+        document.body.style.overflow = ""
+    }
+
+    modalOpenBtns.forEach(btn => {
+        btn.addEventListener("click", openModal)
+    })
+
+    modalCloseBtn.addEventListener("click", closeModal)
+
+    modal.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            closeModal()
+        }
+    })
+
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Escape" && modal.classList.contains("show")) {
+            closeModal()
+        }
+    })
+
+    const modalTimerId = setTimeout(openModal, 4000)
 })
