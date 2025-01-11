2048a6d09becd3bb49323a888c82e89d55ec891a
Farrux Temurov <tfarrux70@gmail.com>
2025-01-11 11:38:04 +0500

    modified the files by implementing carousel

--- the-food-lover-site/js/script.js
+++ the-food-lover-site/js/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 window.addEventListener('DOMContentLoaded', () => {
     // Tabs
 
@@ -48,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2025-09-15'
+    const deadline = '2024-02-01'
 
     function getTimeRemaining(endtime) {
         let days, hours, minutes, seconds
@@ -109,108 +111,239 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline)
 
-    // Modals
+    // Modal
+
     const modalOpenBtns = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector(".modal"),
-        modalCloseBtn = document.querySelector("[data-modal-close]"),
-        modalContent = document.querySelector(".modal__content")
+        modal = document.querySelector('.modal'),
+        modalContent = document.querySelector('.modal__content')
 
     function openModal() {
-        modalContent.classList.add("modal_fade")
-        modal.classList.add("show")
-        modal.classList.remove("hide")
-        document.body.style.overflow = "hidden"
+        modalContent.classList.add('modal_fade')
+        modal.classList.add('show')
+        modal.classList.remove('hide')
+        document.body.style.overflow = 'hidden'
         clearInterval(modalTimerId)
     }
 
     function closeModal() {
-        modal.classList.add("hide")
-        modal.classList.remove("show")
-        document.body.style.overflow = ""
+        modal.classList.add('hide')
+        modal.classList.remove('show')
+        document.body.style.overflow = ''
     }
 
     modalOpenBtns.forEach(btn => {
-        btn.addEventListener("click", openModal)
+        btn.addEventListener('click', openModal)
     })
 
-    modalCloseBtn.addEventListener("click", closeModal)
-
-    modal.addEventListener("click", (event) => {
-        if (event.target === modal) {
+    modal.addEventListener('click', event => {
+        if (
+            event.target === modal ||
+            event.target.getAttribute('data-modal-close') === ''
+        ) {
             closeModal()
         }
     })
 
-    document.addEventListener("keydown", (event) => {
-        if (event.code === "Escape" && modal.classList.contains("show")) {
+    document.addEventListener('keydown', event => {
+        if (event.code === 'Escape' && modal.classList.contains('show')) {
             closeModal()
         }
     })
 
-    const modalTimerId = setTimeout(openModal, 5000)
+    const modalTimerId = setTimeout(openModal, 50000)
 
-    // Offer class
+    // Class
 
     class OfferMenu {
         constructor(src, alt, title, descr, discount, sale, parentSelector) {
-            this.src = src,
-                this.alt = alt,
-                this.title = title,
-                this.descr = descr,
-                this.discount = discount,
-                this.sale = sale,
-                this.parent = document.querySelector(parentSelector),
-                this.formatToUSD()
+            this.src = src
+            this.alt = alt
+            this.title = title
+            this.descr = descr
+            this.discount = discount
+            this.sale = sale
+            this.parent = document.querySelector(parentSelector)
+            this.formatToUSD()
         }
 
         formatToUSD() {
-            this.discount = this.discount.toLocaleString('en-Us', { style: "currency", currency: "USD" })
-            this.sale = this.sale.toLocaleString('en-Us', { style: "currency", currency: "USD" })
+            this.discount = this.discount.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            })
+            this.sale = this.sale.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            })
         }
 
         render() {
             const element = document.createElement('div')
             element.innerHTML = `
-                <img src="${this.src}" alt="${this.alt}">
+				<img src="${this.src}" alt="${this.alt}">
 				<div>
 					<h3>${this.title}</h3>
 					<p>${this.descr}</p>
 					<p><del>${this.discount}</del> <span class="primary-text">${this.sale}</span></p>
 				</div>
-            `
-            this.parent.append(element);
+			`
+
+            this.parent.append(element)
         }
     }
 
-    const offers = [
-        {
-            src: "./img/offer1.png",
-            alt: "Quattro Pasta",
-            title: "Quattro Pasta",
-            descr: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-            discount: 55,
-            sale: 20,
-        },
-        {
-            src: "./img/offer2.png",
-            alt: "Vegertarian Pasta",
-            title: "Vegertarian Pasta",
-            descr: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-            discount: 75,
-            sale: 25,
-        },
-        {
-            src: "./img/offer3.png",
-            alt: "Gluten-Free Pasta",
-            title: "Gluten-Free Pasta",
-            descr: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-            discount: 25,
-            sale: 15,
-        }
-    ]
+    fetch('http://localhost:3000/offers', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(offer => {
+                const { src, alt, descr, discount, sale, title } = offer
+                new OfferMenu(src, alt, title, descr, discount, sale, '.offers-items').render()
+            })
+        })
 
-    offers.forEach(offer => {
-        const { src, alt, descr, discount, sale, title } = offer // destructuring offers array into reusable offer to avoid writing offer.src and so on.
-        new OfferMenu(src, alt, title, descr, discount, sale, ".offers-items").render()
+    // FORM
+
+    const form = document.querySelector('form'),
+        telegramTokenBot = '6582188202:AAHHGOGsOMqUs3ELHhK35it1ykoWYwIo4co',
+        chatId = '5796596917'
+
+    const message = {
+        loading: 'Loading...',
+        success: 'Thanks for contacting with us',
+        failure: 'Something went wrong',
+    }
+
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const loader = document.createElement('div')
+        loader.classList.add('loader')
+        loader.style.width = '20px'
+        loader.style.height = '20px'
+        loader.style.marginTop = '20px'
+        form.append(loader)
+
+        const formData = new FormData(form)
+
+        const object = {}
+        formData.forEach((value, key) => {
+            object[key] = value
+        })
+
+        fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: `Name: ${object.name}. Phone: ${object.phone}`,
+            }),
+        })
+            .then(() => {
+                showStatusMessage(message.success)
+                form.reset()
+            })
+            .catch(() => showStatusMessage(message.failure))
+            .finally(() => loader.remove())
+    })
+
+    function showStatusMessage(message) {
+        const modalDialog = document.querySelector('.modal__dialog')
+
+        modalDialog.classList.add('hide')
+        openModal()
+
+        const statusModal = document.createElement('div')
+        statusModal.classList.add('modal__dialog')
+        statusModal.innerHTML = `
+			<div class="modal__content">
+				<div data-modal-close class="modal__close">&times;</div>
+				<div class="modal__title">${message}</div>
+			</div>
+		`
+
+        document.querySelector('.modal').append(statusModal)
+
+        setTimeout(() => {
+            statusModal.remove()
+            modalDialog.classList.remove('hide')
+            closeModal()
+        }, 4000)
+    }
+
+    // SLIDER
+
+    const slides = document.querySelectorAll(".offer__slide"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current"),
+        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+        slidesInner = document.querySelector(".offer__slider-inner"),
+        width = window.getComputedStyle(slidesWrapper).width
+
+    let slideIndex = 1,
+        offset = 0
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`
+        current.textContent = `0${slideIndex}`
+    } else {
+        total.textContent = slides.length
+        current.textContent = slideIndex
+    }
+
+    slidesInner.style.width = 100 * slides.length + "%"
+    slidesInner.style.display = "flex"
+    slidesInner.style.transition = "all .5s ease"
+
+    slidesWrapper.style.overflow = "hidden"
+
+    slides.forEach(slide => {
+        slide.style.width = width
+    })
+
+    next.addEventListener("click", () => {
+        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0
+        } else {
+            offset += +width.slice(0, width.length - 2)
+        }
+        slidesInner.style.transform = `translateX(-${offset}px)`
+
+        if (slideIndex === slides.length) {
+            slideIndex = 1
+        } else {
+            slideIndex++
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    })
+
+    prev.addEventListener("click", () => {
+        if (offset === 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        } else {
+            offset -= +width.slice(0, width.length - 2)
+        }
+        slidesInner.style.transform = `translateX(-${offset}px)`
+
+        if (slideIndex === 1) {
+            slideIndex = slides.length
+        } else {
+            slideIndex--
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
     })
 })
